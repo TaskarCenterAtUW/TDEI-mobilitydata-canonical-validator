5128f05daeb7d8f6a56eae723f9d42b0d8b03dca
sujata-m <sujatam@gaussiansolutions.com>
2024-10-21 14:10:36 +0530

    Fixed Task-1215
    - Updated mobility endpoint
    - Updated Changelog
    - Updated package version

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ import os
 from setuptools import setup, find_packages, Extension
 from version import version
 
-project_path = os.path.dirname(os.path.realpath(__file__))
-requirements_file = '{}/requirements.txt'.format(project_path)
-
-with open(requirements_file) as f:
-    content = f.readlines()
-install_requires = [x.strip() for x in content]
-
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
@@ -26,7 +19,9 @@ setup(
     },
     long_description_content_type='text/markdown',
     url='https://github.com/TaskarCenterAtUW/TDEI-mobilitydata-canonical-validator',
-    install_requires=install_requires,
+    install_requires=[
+        'requests'
+    ],
     packages=find_packages(where='src'),
     classifiers=[
         'Programming Language :: Python :: 3',
