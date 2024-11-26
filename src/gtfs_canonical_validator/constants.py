9a3d90d5564e9e25bd3820c2bd24db261851e6e1
sujata-m <sujatam@gaussiansolutions.com>
2024-11-26 14:49:45 +0530

    Updated URL

--- src/gtfs_canonical_validator/constants.py
+++ src/gtfs_canonical_validator/constants.py
@@ -1,6 +1,6 @@
 class Constants:
-    JOB_URL = 'https://stg-gtfs-validator-web-mbzoxaljzq-ue.a.run.app/create-job'
-    RESULT_BASE_URL = 'https://staging-gtfs-validator-results.mobilitydata.org'
+    JOB_URL = 'https://gtfs-validator-web-mbzoxaljzq-ue.a.run.app/create-job'
+    RESULT_BASE_URL = 'https://gtfs-validator-results.mobilitydata.org'
     timeout = 10 # 10 seconds timeout
     max_retries = 5
 
